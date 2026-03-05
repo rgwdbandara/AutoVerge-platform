d3888210236d816d47b8a74057aefaf2987227b8
rgwdbandara <rgwdbandara@nsbm.students.ac.lk>
2026-03-05 15:48:39 +0530

    Browse page with filter sidebar and API search integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,34 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import CarDetails from "./pages/CarDetails";
+import BrowseCars from "./pages/BrowseCars";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
 
+
+
       <Routes>
-        <Route path="/" element={<Hero />} />
-        <Route path="/browse" element={<h1>Browse Cars</h1>} />
-        <Route path="/create" element={<h1>Create Listing</h1>} />
-        <Route path="/dashboard" element={<h1>Dashboard</h1>} />
+        <Route path="/" element={<Home />} />
+        <Route
+          path="*"
+          element={
+            <div className="pt-20">
+              <Routes>
+                <Route path="/browse" element={<BrowseCars />} />
+                <Route path="/create" element={<h1>Create Listing</h1>} />
+                <Route path="/dashboard" element={<h1>Dashboard</h1>} />
+                <Route path="/cars/:id" element={<CarDetails />} />
+              </Routes>
+            </div>
+          }
+        />
       </Routes>
 
     </BrowserRouter>
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Hero from "../components/Hero";
+import FeaturedCars from "../components/home/FeaturedCars";
+import BrowseByMake from "../components/home/BrowseByMake";
+
+function Home() {
+  return (
+    <>
+      <Hero />
+      <FeaturedCars />
+      <BrowseByMake />
+    </>
+  );
+}
+
+export default Home;
